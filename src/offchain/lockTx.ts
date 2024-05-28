0383230b2f3dce61107d3744266619d40e7ddfb0
EehMauro <maurojoaquinescudero@gmail.com>
2024-05-27 23:32:31 -0300

    Updated plu-ts version and replaced koios with blockfrost

--- src/offchain/lockTx.ts
+++ src/offchain/lockTx.ts
@@ -1,19 +1,16 @@
-import { Tx } from "@harmoniclabs/plu-ts";
+import { Value, DataB, Address, Tx } from "@harmoniclabs/plu-ts";
 import { BrowserWallet } from "@meshsdk/core";
-import koios from "./koios";
+import { scriptTestnetAddr } from "../../contracts/helloPluts";
+import { toPlutsUtxo } from "./mesh-utils";
+import getTxBuilder from "./getTxBuilder";
+import Blockfrost from "./blockfrost";
 
-async function getLockTx( wallet: BrowserWallet ): Promise<Tx>
-{
-    throw "'lockTx' logic not implemented";
+async function getLockTx(wallet: BrowserWallet): Promise<Tx> {
+  throw new Error("'lockTx' logic not implemented");
 }
 
-export async function lockTx( wallet: BrowserWallet): Promise<string>
-{
-    const unsingedTx = await getLockTx( wallet );
-
-    const txStr = await wallet.signTx(
-        unsingedTx.toCbor().toString()
-    );
-
-    return (await koios.tx.submit( Tx.fromCbor( txStr ) as any )).toString();
+export async function lockTx( wallet: BrowserWallet): Promise<string> {
+  const unsingedTx = await getLockTx(wallet);
+  const txStr = await wallet.signTx(unsingedTx.toCbor().toString());
+  return await Blockfrost.submitTx(txStr);
 }
--- src/offchain/unlockTx.ts
+++ src/offchain/unlockTx.ts
@@ -1,20 +1,22 @@
-import { Tx } from "@harmoniclabs/plu-ts";
+import { Address, isData, DataB, Tx } from "@harmoniclabs/plu-ts";
+import { fromAscii, uint8ArrayEq } from "@harmoniclabs/uint8array-utils";
 import { BrowserWallet } from "@meshsdk/core";
-import { koios } from "./koios";
+import { script, scriptTestnetAddr } from "../../contracts/helloPluts";
+import { toPlutsUtxo } from "./mesh-utils";
+import getTxBuilder from "./getTxBuilder";
+import Blockfrost from "./blockfrost";
 
-async function getUnlockTx( wallet: BrowserWallet ): Promise<Tx>
-{
-    throw "'unlockTx' logic not implemented";   
+async function getUnlockTx(wallet: BrowserWallet): Promise<Tx> {
+  throw new Error("'unlockTx' logic not implemented");   
 }
 
-export async function unlockTx( wallet: BrowserWallet ): Promise<string>
-{
-    const unsingedTx = await getUnlockTx( wallet );
+export async function unlockTx(wallet: BrowserWallet): Promise<string> {
+  const unsingedTx = await getUnlockTx(wallet);
 
-    const txStr = await wallet.signTx(
-        unsingedTx.toCbor().toString(),
-        true // partial sign because we have smart contracts in the transaction
-    );
+  const txStr = await wallet.signTx(
+    unsingedTx.toCbor().toString(),
+    true // partial sign because we have smart contracts in the transaction
+  );
 
-    return (await koios.tx.submit( Tx.fromCbor( txStr ) )).toString();
+  return await Blockfrost.submitTx(txStr);
 }
